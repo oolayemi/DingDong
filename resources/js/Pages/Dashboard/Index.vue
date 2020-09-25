9b5c77c0e2fd1786d1547b20ffdcb074095906ed
oolayemi <olayemiolaomo5@gmail.com>
2020-09-25 23:36:57 +0100

    ...

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="mb-8 font-bold text-3xl ">Dashboard</h1>
+    <h1 class="mb-8 font-bold text-3xl">Dashboard</h1>
     <p class="mb-8 leading-normal">Hey there! Welcome to Ping CRM, a demo app designed to help illustrate how <a class="text-indigo-500 underline hover:text-orange-600" href="https://inertiajs.com">Inertia.js</a> works.</p>
     <div class="mb-8 flex">
       <inertia-link class="btn-indigo" href="/500">500 error</inertia-link>
@@ -11,7 +11,7 @@
 </template>
 
 <script>
-import Layout from '@/Shared/Layout'
+import Layout from '@/Shared/Layout';
 
 export default {
   metaInfo: { title: 'Dashboard' },
