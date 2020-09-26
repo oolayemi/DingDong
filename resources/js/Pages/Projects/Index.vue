c20fbc2db47a527b41ff9da23131e0202a128f9d
oolayemi <olayemiolaomo5@gmail.com>
2020-09-26 19:09:24 +0100

    updated

--- resources/js/Pages/Projects/Index.vue
+++ resources/js/Pages/Projects/Index.vue
@@ -1,6 +1,5 @@
 <template>
     <h1 class="mb-8 font-bold text-3xl"> Projects </h1>
-
 </template>
 
 <script>
--- resources/js/Shared/Player.vue
+++ resources/js/Shared/Player.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex-shrink-0 bg-white border-t p-4">
       <audio controls class="w-full">
-          <source src="public/mp3/let.mp3" type="audio/mp3" />
+          <source src="public/mp3/lie.mp3" type="audio/mp3" />
       </audio>
     </div>
 </template>
